9a2ca64b3490eaed87c3a290b792188f1d23e02a
samun <ahmadjailan2012@gmail.com>
2026-01-02 21:40:25 +0430

    fixed password

--- components/ui/login-form.tsx
+++ components/ui/login-form.tsx
@@ -142,15 +142,15 @@ export function LoginForm({
               )}
 
               <Field>
-                {/* <div className="flex items-center">
+                <div className="flex items-center">
                   <FieldLabel htmlFor="password">Password</FieldLabel>
-                  <a
+                  {/* <a
                     href="#"
                     className="ml-auto inline-block text-sm underline-offset-4 hover:underline"
                   >
                     Forgot your password?
-                  </a>
-                </div> */}
+                  </a> */}
+                </div>
 
                 <Input
                   id="password"
